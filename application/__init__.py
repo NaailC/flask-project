bbec14ba825d804d5f3b9820d70d71995b682c27
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-15 16:23:00 +0000

    commit

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from wtforms import StringField, SubmitField
 
 # Use Flask framework and configure database, forms for App
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@35.246.119.161/dnbdatabase"
 #external config: mysql+pymysql://root:root@35.246.119.161/dnbdatabase
 app.config["SECRET_KEY"] = "bcxvbvcxdfstrqlitjgdsabbxcv"
 
