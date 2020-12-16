587392932dcff886c6f80ccccdceab6a8f71fb4c
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-16 09:50:43 +0000

    update

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,10 +9,3 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from application import app, db, bcrypt
 from application.models import Users
-
-class TestViews(TestBase):
-#TEST JENKINS SERVER
-    def test_home_get(self):
-
-        response = self.client.get(url_for('home'))
-        self.assertEqual(response.status_code, 200)
