7e840837777d9a1ebecdf93409d4b5feff4312b7
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-16 12:50:11 +0000

    Artist page work

--- application/models.py
+++ application/models.py
@@ -5,13 +5,13 @@ from datetime import datetime
 
 # Create Artist Class and Table and allow a relationship 
 class Artist(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    artist_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     artist_name = db.Column(db.String(30), nullable=False, unique=True)
 
 
 # Create Songs Class and Table  
 class Songs(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    song_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     song_name = db.Column(db.String(60), nullable=False)
-    artist_id = db.Column(db.ForeignKey('Artist.id'), nullable=False)
+    artist_id = db.Column(db.ForeignKey('Artist.artist_id'), nullable=False)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
