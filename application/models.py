e494a427e554c5fa32776222e2db6c3f8ad55855
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-17 10:48:51 +0000

    please work :( naail

--- application/models.py
+++ application/models.py
@@ -5,17 +5,19 @@ from datetime import datetime
 
 # Create Artist Class and Table and allow a relationship 
 class Artist(db.Model):
+
     __tablename__= 'artist'
-    artist_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     artist_name = db.Column(db.String(30), nullable=False, unique=True)
-    song_name = db.relationship("Songs", backref=db.backref("songs", uselist=False))
+    song_name = db.relationship("Songs", backref=db.backref("songs", lazy='dynamic'))
 
 # Create Songs Class and Table  
 class Songs(db.Model):
+
     __tablename__= 'songs'
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     song_name = db.Column(db.String(60), nullable=False)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    artists = db.relationship("Artist", backref=db.backref("artist", uselist=False))
+    artists = db.relationship("Artist", backref=db.backref("artist", lazy='dynamic'))
