94dfb2166b0fd2f2cbfe16025144375575310d0d
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-30 17:39:07 +0000

    database init files

--- application/models.py
+++ application/models.py
@@ -10,14 +10,7 @@ class Artist(db.Model):
     __tablename__ = 'artist'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     artist_name = db.Column(db.String(30), nullable=False, unique=True)
-
-    tunes = db.relationship("Song", backref='artist')
-
-    def __init__(self, artist_name):
-        self.artist_name = artist_name
-
-    def __repr__(self):
-        return '<Artist %r>' % self.id
+    tunes = db.relationship("Song", backref='artist', lazy='dynamic')
 
     
 # Create Songs Class and Table  
@@ -27,13 +20,5 @@ class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     song_name = db.Column(db.String(60), nullable=False)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.artist_id'))
-    artist = db.relationship('movie')
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
     
-    def __init__(self, song_name):
-        self.song_name = song_name
-        self.artist = artist
-
-    def __repr__(self):
-        return '<Song %r>' % self.id
