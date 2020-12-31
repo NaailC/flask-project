4073d20ee081d683e28dda368df1af67602af9a9
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-31 03:44:23 +0000

    hopefully schema works

--- application/models.py
+++ application/models.py
@@ -10,17 +10,17 @@ class Artist(db.Model):
     __tablename__ = 'artist'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     artist_name = db.Column(db.String(30), nullable=False, unique=True)
-    songs = db.relationship('Song', backref='artist', lazy='dynamic')
+    songs = db.relationship('Song', backref='artist', lazy='True')
 
     
 # Create Songs Class and Table  
 class Song(db.Model):
 
     __tablename__ = 'song'
-    artist = db.relationship('Artist')
+    
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     song_name = db.Column(db.String(60), nullable=False)
     date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    artistid = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
-    
+    artist = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
+    artistid = db.relationship('Artist')
     
