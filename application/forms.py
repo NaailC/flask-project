01b60e58b2533a29d1b19507f29f64920c951fcb
naailchoudhury@gmail.com <naailchoudhury@gmail.com>
2020-12-31 04:14:14 +0000

    models

--- application/forms.py
+++ application/forms.py
@@ -8,7 +8,7 @@ from application.models import Song, Artist
 # Create Form Class to Update Song Name CRUD app 
 class SongForm(FlaskForm):
     songname = StringField('Song Name', validators=[DataRequired()])
-    artist = SelectField('Artist Name', choices=[(artist.id, artist.artistname)], validators=[DataRequired()])
+    artist = SelectField('Artist Name', choices=[(Artist.id, Artist.artistname)], validators=[DataRequired()])
     submitsong = SubmitField('Accept')
 
 class ArtistForm(FlaskForm):
