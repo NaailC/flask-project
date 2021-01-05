efeabc84b5a3ae44d954477de38a67a9f6fa543b
NaailC <naailchoudhury@gmail.com>
2021-01-05 10:40:54 +0000

    readme

--- startup.sh
+++ startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd /opt/flask-project/application
+
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 create.py
-sudo systemctl stop app.service
 sudo systemctl daemon-reload
 pytest --cov=application --cov-report html
